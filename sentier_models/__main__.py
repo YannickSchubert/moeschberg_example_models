ad3d53618893dd4359acb65ec605915cdb642182
Holger Finger <holgerfinger@protonmail.com>
2024-06-27 11:34:27 +0200

    dataclasses

--- sentier_models/__main__.py
+++ sentier_models/__main__.py
@@ -2,13 +2,15 @@ from sentier_models.abstract_model.attribute import Attribute
 from sentier_models.abstract_model.product import Product
 from sentier_glossary import GlossaryAPI, CommonSchemes
 
+from sentier_models.wind_turbine_design.wind_turbine_design import WindTurbineDesign
+
 if __name__ == "__main__":
 
     api = GlossaryAPI()
 
-    print(api.schemes())
-
-    print(api.semantic_search("piggies", CommonSchemes.cn2024))
+    # TODO: find winc turbine in glossary...
+    #print(api.schemes())
+    #print(api.semantic_search("piggies", CommonSchemes.cn2024))
 
     wind_turbine = Product(
         uri="wind_turbine",
@@ -19,5 +21,12 @@ if __name__ == "__main__":
             Attribute(uri="location", value="denmark", unit="location"),
         ],
     )
-
     print(wind_turbine)
+
+    # TODO: orchestrator should find the model based on the product
+
+    model = WindTurbineDesign()
+    model.inputs = [wind_turbine]
+    model.run()
+
+    print(model.outputs)
